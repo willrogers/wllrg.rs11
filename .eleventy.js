0e266df342334d39c76968af9d44a8b0feee6d62
Will Rogers <wahrogers@gmail.com>
2023-03-16 21:19:24 +0000

    Use path prefix.

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,7 @@ module.exports = function (eleventyConfig) {
   });
 
   return {
+    pathPrefix: "/wllrg.rs11/",
     dir: {
       input: "content",         // default: "."
       includes: "../_includes",  // default: "_includes"
